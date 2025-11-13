74b25b414beb3065517d1455f19be2621ee9d6ca
Adam White <hello@adamwhite.work>
2025-11-12 22:45:44 -0600

    chore: Clean up

--- archive/ui-app-vite-design-reference/components/EventTimeline/index.ts
+++
@@ -1,8 +0,0 @@
-/**
- * EventTimeline Component Exports
- * Story 7.3: Event Timeline Canvas
- */
-
-export { EventTimelineCanvas } from './EventTimelineCanvas';
-export { StageCard } from './StageCard';
-export { StageLogDrawer } from './StageLogDrawer';
